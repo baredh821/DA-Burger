c65a6474230b8351fa751dc79ac56a67c6fd4793
baredh821 <baredh821@gmail.com>
2019-12-19 14:27:55 -0600

    Files Updated

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ var express = require("express");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
+var exphbs = require("express-handlebars")
 
 // Requiring our models for syncing
-var db = require("./models");
+//var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -12,15 +13,18 @@ app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
+// require("./controllers/burgerControllers.js")(app);
+var routes = require("./controllers/burgerControllers");
+app.use(routes)
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
-db.sequelize.sync().then(function() {
+
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
